6fbdf22db664e43faf2ae576c77245b53df3b47c
HoNooD <honood@gmail.com>
2024-12-12 22:49:27 +0800

    day 7 solution (part 2) (poor performance)

--- day_07/main.cpp
+++ day_07/main.cpp
@@ -2,31 +2,35 @@
 //
 // clang++ -std=gnu++20 -stdlib=libc++ main.cpp -o main && ./main && rm main
 
+#include <cstdint>
 #include <fstream>
 #include <sstream>
 #include <vector>
 #include <utility>
 #include <functional>
 #include <cassert>
+#include <cmath>
 #include <iostream>
 
 #include "../aoc.h"
 
-std::vector<std::pair<long, std::vector<long>>> parse_input() {
+using u64 = uint64_t;
+
+std::vector<std::pair<u64, std::vector<u64>>> parse_input() {
   std::ifstream ifs{helper::read_file_as_stream("input.txt")};
   std::istringstream iss{};
 
-  std::vector<std::pair<long, std::vector<long>>> res{};
+  std::vector<std::pair<u64, std::vector<u64>>> res{};
 
   std::string line{};
   while (std::getline(ifs, line)) {
     auto colon_pos = line.find(':');
-    long test_val = std::stol(line.substr(0, colon_pos));
+    u64 test_val = std::stol(line.substr(0, colon_pos));
 
     iss.str(line.substr(colon_pos + 1));
     iss.clear();
-    long operand = 0;
-    std::vector<long> operands{};
+    u64 operand = 0;
+    std::vector<u64> operands{};
     while (iss >> operand) {
       operands.emplace_back(operand);
     }
@@ -37,12 +41,26 @@ std::vector<std::pair<long, std::vector<long>>> parse_input() {
   return res;
 }
 
-using op_t = std::function<long(long, long)>;
+u64 concat_nums(u64 high, u64 low) {
+  if (high == 0) { return low; }
+  if (low == 0) { return high * 10; }
+  
+  u64 temp = low;
+  int low_digits = 0;
+  do {
+    ++low_digits;
+    temp /= 10;
+  } while (temp > 0);
+  
+  while (low_digits-- > 0) { high *= 10; }
+  
+  return high + low;
+};
 
-std::vector<std::vector<op_t>> gen_all_op_seq(int const seq_len) {
-  std::vector<std::vector<op_t>> res{};
-  res.reserve(1 << seq_len);
-  std::vector<op_t> curr{};
+std::vector<std::string> gen_all_op_seq(int const seq_len, bool for_part2 = false) {
+  std::vector<std::string> res{};
+  res.reserve(std::pow(for_part2 ? 3 : 2, seq_len));
+  std::string curr{};
 
   std::function<void()> backtrack = [&] {
     if (curr.size() == seq_len) {
@@ -50,13 +68,19 @@ std::vector<std::vector<op_t>> gen_all_op_seq(int const seq_len) {
       return;
     }
 
-    curr.emplace_back(std::plus<long>{});
+    curr.push_back('+');
     backtrack();
     curr.pop_back();
 
-    curr.emplace_back(std::multiplies<long>{});
+    curr.push_back('*');
     backtrack();
     curr.pop_back();
+
+    if (for_part2) {
+      curr.push_back('|');
+      backtrack();
+      curr.pop_back();
+    }
   };
 
   backtrack();
@@ -64,15 +88,20 @@ std::vector<std::vector<op_t>> gen_all_op_seq(int const seq_len) {
   return res;
 }
 
-long calc_part1_total_calibration(std::vector<std::pair<long, std::vector<long>>> const& input_data) {
-  long sum = 0;
+u64 calc_total_calibration(std::vector<std::pair<u64, std::vector<u64>>> const& input_data, bool for_part2 = false) {
+  u64 sum = 0;
 
   for (auto const& [test_val, operands] : input_data) {
     bool found = false;
-    for (auto const& op_seq : gen_all_op_seq(operands.size() - 1)) {
-      long temp = operands[0];
+    for (auto const& op_seq : gen_all_op_seq(operands.size() - 1, for_part2)) {
+      u64 temp = operands[0];
       for (int i = 1; i < operands.size(); ++i) {
-        temp = op_seq[i - 1](temp, operands[i]);
+        switch (op_seq[i - 1]) {
+          case '+': temp += operands[i]; break;
+          case '*': temp *= operands[i]; break;
+          case '|': temp = concat_nums(temp, operands[i]); break;
+          default: assert(false);
+        }
       }
 
       if (temp == test_val) {
@@ -92,7 +121,11 @@ long calc_part1_total_calibration(std::vector<std::pair<long, std::vector<long>>
 int main() {
   auto input_data = parse_input();
 
-  long part1_total_calibration = calc_part1_total_calibration(input_data);
+  u64 part1_total_calibration = calc_total_calibration(input_data);
   assert(2501605301465 == part1_total_calibration);
   std::cout << "Part 1: What is their total calibration result? " << part1_total_calibration << '\n';
+
+  u64 part2_total_calibration = calc_total_calibration(input_data, true);
+  assert(44841372855953 == part2_total_calibration);
+  std::cout << "Part 2: What is their total calibration result? " << part2_total_calibration << '\n';
 }
