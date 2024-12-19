ccfcce32d443846da7834570f88485aef313ec7e
HoNooD <honood@gmail.com>
2024-12-19 22:40:35 +0800

    day 17 solution (part 2) (harder)

--- 2024/day_17/main.cpp
+++ 2024/day_17/main.cpp
@@ -2,13 +2,14 @@
 //
 // clang++ -std=gnu++20 -stdlib=libc++ main.cpp -o main && ./main && rm main
 
+#include <cstdint>
 #include <vector>
 #include <unordered_map>
 #include <functional>
-#include <cstdint>
-#include <iostream>
 #include <stdexcept>
+#include <algorithm>
 #include <cassert>
+#include <iostream>
 
 using u64 = uint64_t;
 
@@ -39,18 +40,63 @@ void solve_part1() {
     {7u, [&](u64 operand) { cr = ar >> combo_operand(operand); }},
   };
 
-  while (ip < prog.size() - 1) {
-    opcode2insts[prog[ip++]](prog[ip++]);
-  }
+  while (ip < prog.size() - 1) { opcode2insts[prog[ip++]](prog[ip++]); }
 
+  // assert((res == std::vector<u64>{3, 1, 4, 3, 1, 7, 1, 6, 3}));
+  std::cout << "Part 1: What do you get if you use commas to join the values it output into a single string? ";
   for (int i = 0; i < res.size(); ++i) {
-    if (i > 0) { std::cout << ','; }
-    std::cout << res[i];
+    std::cout << res[i] << (i == res.size() - 1 ? '\n' : ',');
   }
-  std::cout << '\n';
-  assert((res == std::vector<u64>{3,1,4,3,1,7,1,6,3}));
+}
+
+void solve_part2() {
+  std::vector<u64> const prog{2, 4, 1, 2, 7, 5, 4, 5, 1, 3, 5, 5, 0, 3, 3, 0};
+  // For the input `prog`, the following instructions will be executed one by one:
+  //
+  // 1) 2,4: br = ar & 7
+  // 2) 1,2: br = br ^ 2
+  // 3) 7,5: cr = ar >> br
+  // 4) 4,5: br = br ^ cr
+  // 5) 1,3: br = br ^ 3
+  // 6) 5,5: OUTPUT br & 7
+  // 7) 0,3: ar = ar >> 3
+  // 8) 3,0: if ar == 0 then EXIT else GOTO prog[0]
+  //
+  // which are equivalent to:
+  //
+  // 1) 5:5: OUTPUT ((ar & 7 ^ 2) ^ (ar >> (ar & 7 ^ 2)) ^ 3) & 7
+  // 2) 0,3: ar = ar >> 3
+  // 3) 3,0: if ar == 0 then EXIT else GOTO [0]
+
+  u64 lowest_init_ar = -1;
+
+  auto output = [](u64 ar) { return ((ar & 7 ^ 2) ^ (ar >> (ar & 7 ^ 2)) ^ 3) & 7; };
+
+  std::function<void(int, u64)> backtrack = [&](int index, u64 ar) {
+    if (index == -1) {
+      lowest_init_ar = std::min(lowest_init_ar, ar);
+      return;
+    }
+
+    for (u64 delta = 0b000; delta <= 0b111; ++delta) {
+      // Register A must be non-zero before outputting the final value. Otherwise,
+      // the last jump instruction will fail to return to the start, and the final
+      // value will not be output.
+      if (index == prog.size() - 1 && delta == 0b000) { continue; }
+
+      u64 ar_before_output = (ar << 3) + delta;
+      if (prog[index] == output(ar_before_output)) {
+        backtrack(index - 1, ar_before_output);
+      }
+    }
+  };
+
+  backtrack(prog.size() - 1, 0);
+  assert(37221270076916 == lowest_init_ar);
+  std::cout << "Part 2: What is the lowest positive initial value for register A that causes the program to output a copy of itself? " << lowest_init_ar << '\n';
 }
 
 int main() {
   solve_part1();
+  solve_part2();
 }
